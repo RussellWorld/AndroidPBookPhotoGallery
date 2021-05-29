d30f5609bae6fad41329b10ad1d452dc8a890752
RassellWorld <miraqq55@gmail.com>
2021-05-29 04:04:14 +0200

    Theme 28

---
+++ app/src/main/java/com/example/photogallery/NotificationReceiver.kt
@@ -0,0 +1,14 @@
+package com.example.photogallery
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+
+private const val TAG = "NotificationReceiver"
+
+class NotificationReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        Log.i(TAG, "received breadcast: ${intent?.action}")
+    }
+}
--- app/src/main/java/com/example/photogallery/PollWorker.kt
+++ app/src/main/java/com/example/photogallery/PollWorker.kt
@@ -2,6 +2,7 @@ package com.example.photogallery
 
 import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.util.Log
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
@@ -58,9 +59,15 @@ class PollWorker(val context: Context, workerParameters: WorkerParameters) :
             val notificationManager = NotificationManagerCompat.from(context)
             notificationManager.notify(0, notification)
 
+            context.sendBroadcast(Intent(ACTION_SHOW_NOTIFICATION), PERM_PRIVATE)
         }
 
         return Result.success()
 
+    }
+
+    companion object{
+        const val ACTION_SHOW_NOTIFICATION = "com.example.photogallery.SHOW_NOTIFICATION"
+        const val PERM_PRIVATE = "com.example.photogallery.PRIVATE"
     }
 }
